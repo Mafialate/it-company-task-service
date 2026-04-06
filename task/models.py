b5aeaf68258225aa6db3df79ffd2b4fd099cf060
Mafialate <mafialate@ukr.net>
2026-04-06 09:54:53 +0200

    feat: add Task, Worker, TaskType, Position models and migrations

--- task/models.py
+++ task/models.py
@@ -1,3 +1,58 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from it_company_task_service import settings
+
+
+class Position(models.Model):
+    name = models.CharField(
+        max_length=255,
+        unique=True,
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class TaskType(models.Model):
+    name = models.CharField(
+        max_length=255,
+        unique=True,
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(
+        Position,
+        on_delete=models.PROTECT,
+        related_name='workers'
+    )
+
+    def __str__(self):
+        return f"{self.username} {self.position}"
+
+
+class Task(models.Model):
+    class Priority(models.TextChoices):
+        URGENT = "urgent", "Urgent"
+        HIGH = "high", "High"
+        MEDIUM = "medium", "Medium"
+        LOW = "low", "Low"
+
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    deadline = models.DateField()
+    is_completed = models.BooleanField(default=False)
+    priority = models.CharField(
+        max_length=10,
+        choices=Priority.choices,
+        default=Priority.MEDIUM,
+    )
+    task_type = models.ForeignKey(TaskType, on_delete=models.PROTECT, related_name="tasks")
+    assignees = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='tasks')
+
+    def __str__(self):
+        return f"{self.name} {self.priority}"
