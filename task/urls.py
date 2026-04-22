7fcac41563b5e339b525872efd090c6cd5135743
Mafialate <mafialate@ukr.net>
2026-04-22 11:42:33 +0200

    Complete business logic: forms, views and URL routing

---
+++ task/urls.py
@@ -0,0 +1,57 @@
+from django.urls import path
+
+from task.views import (
+    index,
+    TaskTypeListView,
+    TaskTypeDeleteView,
+    TaskTypeUpdateView,
+    TaskTypeCreateView, TaskListView, TaskUpdateView, TaskDeleteView, TaskCreateView, TaskDetailView, WorkerListView,
+    WorkerCreateView, WorkerUpdateView, WorkerDeleteView, WorkerDetailView, PositionListView, PositionCreateView,
+    PositionUpdateView, PositionDeleteView, TeamListView, TeamCreateView, ProjectListView, ProjectCreateView,
+    TeamDetailView, TeamUpdateView, TeamDeleteView, ProjectDetailView, ProjectUpdateView, ProjectDeleteView, contact_us,
+    privacy
+)
+
+urlpatterns = [
+    path("", index, name='index'),
+#TaskType
+    path("task-type/", TaskTypeListView.as_view(), name='task-type-list'),
+    path("task-type/create/", TaskTypeCreateView.as_view(), name='task-type-create'),
+    path("task-type/<int:pk>/update/", TaskTypeUpdateView.as_view(), name='task-type-update'),
+    path("task-type/<int:pk>/delete/", TaskTypeDeleteView.as_view(), name='task-type-delete'),
+#Task
+    path("task/", TaskListView.as_view(), name='task-list'),
+    path("task/<int:pk>/", TaskDetailView.as_view(), name='task-detail'),
+    path("task/create/", TaskCreateView.as_view(), name='task-create'),
+    path("task/<int:pk>/update/", TaskUpdateView.as_view(), name='task-update'),
+    path("task/<int:pk>/delete/", TaskDeleteView.as_view(), name='task-delete'),
+#Worker
+    path("worker/", WorkerListView.as_view(), name='worker-list'),
+    path("worker/<int:pk>/", WorkerDetailView.as_view(), name='worker-detail'),
+    path("worker/create/", WorkerCreateView.as_view(), name='worker-create'),
+    path("worker/<int:pk>/update/", WorkerUpdateView.as_view(), name='worker-update'),
+    path("worker/<int:pk>/delete/", WorkerDeleteView.as_view(), name='worker-delete'),
+#Position
+    path("position/", PositionListView.as_view(), name='position-list'),
+    path("position/create/", PositionCreateView.as_view(), name='position-create'),
+    path("position/<int:pk>/update/", PositionUpdateView.as_view(), name='position-update'),
+    path("position/<int:pk>/delete/", PositionDeleteView.as_view(), name='position-delete'),
+#Team
+    path("team/", TeamListView.as_view(), name='team-list'),
+    path("team/<int:pk>/", TeamDetailView.as_view(), name='team-detail'),
+    path("team/create/", TeamCreateView.as_view(), name='team-create'),
+    path("team/<int:pk>/update/", TeamUpdateView.as_view(), name='team-update'),
+    path("team/<int:pk>/delete/", TeamDeleteView.as_view(), name='team-delete'),
+#Project
+    path("project/", ProjectListView.as_view(), name='project-list'),
+    path("project/<int:pk>/", ProjectDetailView.as_view(), name='project-detail'),
+    path("project/create/", ProjectCreateView.as_view(), name='project-create'),
+    path("project/<int:pk>/update/", ProjectUpdateView.as_view(), name='project-update'),
+    path("project/<int:pk>/delete/", ProjectDeleteView.as_view(), name='project-delete'),
+#Contact us
+    path("contact-us/", contact_us, name='contact-us'),
+#Privacy
+    path("privacy/", privacy, name='privacy'),
+]
+
+app_name = "task"
--- task/views.py
+++ task/views.py
@@ -1,3 +1,286 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
 
-# Create your views here.
+from task.forms import TaskTypesSearchForm, PositionSearchForm, WorkerSearchForm, WorkerCreateForm, \
+    WorkerPositionUpdateForm, ProjectSearchForm, ProjectUpdateForm, TaskSearchForm, PositionUpdateForm, TaskCreateForm, \
+    TaskUpdateForm, TeamSearchForm, TeamCreateForm, TeamUpdateForm, ProjectCreateForm
+from task.models import Position, TaskType, Task, Team, Project
+
+
+@login_required
+def index(request: HttpRequest) -> HttpResponse:
+    tasks_num = Task.objects.count()
+    task_types_num = TaskType.objects.count()
+    workers_num = get_user_model().objects.count()
+    positions_num = Position.objects.count()
+    projects_num = Project.objects.count()
+    teams_num = Team.objects.count()
+
+    context = {
+        "tasks_num": tasks_num,
+        "task_types_num": task_types_num,
+        "workers_num": workers_num,
+        "positions_num": positions_num,
+        "projects_num": projects_num,
+        "teams_num": teams_num,
+    }
+
+    return render(request, "task/index.html", context=context)
+
+#Contact us page
+@login_required
+def contact_us(request: HttpRequest) -> HttpResponse:
+    return render(request, "task/page-contact-us.html")
+
+#Privacy page
+@login_required
+def privacy(request: HttpRequest) -> HttpResponse:
+    return render(request, "task/page-privacy.html")
+
+#TaskType CRUD
+class TaskTypeListView(LoginRequiredMixin, ListView):
+    model = TaskType
+    template_name = "task/task_type_list.html"
+    paginate_by = 2
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["search_form"] = TaskTypesSearchForm(self.request.GET)
+
+        return context
+
+    def get_queryset(self):
+        queryset = TaskType.objects.all()
+        query = self.request.GET.get("name")
+
+        if query:
+            return queryset.filter(name__icontains=query)
+
+        return queryset
+
+class TaskTypeCreateView(LoginRequiredMixin, CreateView):
+    model = TaskType
+    fields = "__all__"
+    template_name = "task/task_type_form.html"
+    success_url = reverse_lazy("task:task-type-list")
+
+class TaskTypeUpdateView(LoginRequiredMixin, UpdateView):
+    model = TaskType
+    fields = "__all__"
+    template_name = "task/task_type_form.html"
+    success_url = reverse_lazy("task:task-type-list")
+
+class TaskTypeDeleteView(LoginRequiredMixin, DeleteView):
+    model = TaskType
+    template_name = "task/task_type_confirm_delete.html"
+    success_url = reverse_lazy("task:task-type-list")
+
+#Task CRUD
+class TaskListView(LoginRequiredMixin, ListView):
+    model = Task
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["search_form"] = TaskSearchForm(self.request.GET)
+
+        return context
+
+    def get_queryset(self):
+        queryset = (
+            Task.objects.all()
+            .select_related("project", "task_type")
+            .prefetch_related("workers")
+        )
+        query = self.request.GET.get("name")
+
+        if query:
+            return queryset.filter(name__icontains=query)
+
+        return queryset
+
+
+class TaskDetailView(LoginRequiredMixin, DetailView):
+    model = Task
+
+
+class TaskCreateView(LoginRequiredMixin, CreateView):
+    model = Task
+    form_class = TaskCreateForm
+    success_url = reverse_lazy("task:task-list")
+
+
+class TaskUpdateView(LoginRequiredMixin, UpdateView):
+    model = Task
+    form_class = TaskUpdateForm
+    success_url = reverse_lazy("task:task-list")
+
+
+class TaskDeleteView(LoginRequiredMixin, DeleteView):
+    model = Task
+    success_url = reverse_lazy("task:task-list")
+
+#Worker CRUD
+class WorkerListView(LoginRequiredMixin, ListView):
+    model = get_user_model()
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["search_form"] = WorkerSearchForm(self.request.GET)
+
+        return context
+
+    def get_queryset(self):
+        queryset = get_user_model().objects.all().select_related("position")
+        query = self.request.GET.get("username")
+
+        if query:
+            return queryset.filter(username__icontains=query)
+
+        return queryset
+
+class WorkerDetailView(LoginRequiredMixin, DetailView):
+    model = get_user_model()
+    fields = "__all__"
+
+    def get_queryset(self):
+        return get_user_model().objects.all().prefetch_related("teams", "tasks").all()
+
+class WorkerCreateView(LoginRequiredMixin, CreateView):
+    model = get_user_model()
+    form_class = WorkerCreateForm
+    success_url = reverse_lazy("task:worker-list")
+
+class WorkerUpdateView(LoginRequiredMixin, UpdateView):
+    model = get_user_model()
+    form_class = WorkerPositionUpdateForm
+    success_url = reverse_lazy("task:worker-list")
+
+
+class WorkerDeleteView(LoginRequiredMixin, DeleteView):
+    model = get_user_model()
+    success_url = reverse_lazy("task:worker-list")
+
+#Position CRUD
+class PositionListView(LoginRequiredMixin, ListView):
+    model = Position
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["search_form"] = PositionSearchForm(self.request.GET)
+
+        return context
+
+    def get_queryset(self):
+        queryset = Position.objects.all()
+        query = self.request.GET.get("name")
+
+        if query:
+            return queryset.filter(name__icontains=query)
+
+        return queryset
+
+
+class PositionCreateView(LoginRequiredMixin, CreateView):
+    model = Position
+    fields = "__all__"
+    success_url = reverse_lazy("task:position-list")
+
+
+class PositionUpdateView(LoginRequiredMixin, UpdateView):
+    model = Position
+    form_class = PositionUpdateForm
+    success_url = reverse_lazy("task:position-list")
+
+
+class PositionDeleteView(LoginRequiredMixin, DeleteView):
+    model = Position
+    success_url = reverse_lazy("task:position-list")
+
+#Team CRUD
+class TeamListView(LoginRequiredMixin, ListView):
+    model = Team
+    form_class = TeamSearchForm
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["search_form"] = TeamSearchForm(self.request.GET)
+
+        return context
+
+    def get_queryset(self):
+        queryset = Team.objects.all().prefetch_related("workers")
+        query = self.request.GET.get("name")
+
+        if query:
+            return queryset.filter(name__icontains=query)
+
+        return queryset
+
+
+class TeamDetailView(LoginRequiredMixin, DetailView):
+    model = Team
+    fields = "__all__"
+
+
+class TeamCreateView(LoginRequiredMixin, CreateView):
+    model = Team
+    form_class = TeamCreateForm
+    success_url = reverse_lazy("task:team-list")
+
+
+class TeamUpdateView(LoginRequiredMixin, UpdateView):
+    model = Team
+    form_class = TeamUpdateForm
+    success_url = reverse_lazy("task:team-list")
+
+
+class TeamDeleteView(LoginRequiredMixin, DeleteView):
+    model = Team
+    success_url = reverse_lazy("task:team-list")
+
+#Project CRUD
+class ProjectListView(LoginRequiredMixin, ListView):
+    model = Project
+    queryset = Project.objects.all().prefetch_related("teams")
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["search_form"] = ProjectSearchForm(self.request.GET)
+
+        return context
+
+    def get_queryset(self):
+        queryset = Project.objects.all()
+        query = self.request.GET.get("name")
+
+        if query:
+            return queryset.filter(name__icontains=query)
+
+        return queryset
+
+
+class ProjectDetailView(LoginRequiredMixin, DetailView):
+    model = Project
+    fields = "__all__"
+
+
+class ProjectCreateView(LoginRequiredMixin, CreateView):
+    model = Project
+    form_class = ProjectCreateForm
+    success_url = reverse_lazy("task:project-list")
+
+
+class ProjectUpdateView(LoginRequiredMixin, UpdateView):
+    model = Project
+    form_class = ProjectUpdateForm
+    success_url = reverse_lazy("task:project-list")
+
+
+class ProjectDeleteView(LoginRequiredMixin, DeleteView):
+    model = Project
+    success_url = reverse_lazy("task:project-list")
