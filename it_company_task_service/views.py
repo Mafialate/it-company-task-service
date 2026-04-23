ce22038ab3c3a8847705ab0a0632aee041420213
Mafialate <mafialate@ukr.net>
2026-04-23 10:41:16 +0200

    Move TaskService.png and img.png to the docs folder

--- it_company_task_service/views.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth import authenticate
-from django.contrib.auth.decorators import login_required
-from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
-
-
-def register(request):
-    return None
