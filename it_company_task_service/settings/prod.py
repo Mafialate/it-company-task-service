cb9cc5f32a66e540de10f48a911d964ccad1aeda
Mafialate <mafialate@ukr.net>
2026-05-02 10:56:35 +0200

    Update settings.prod and requirements

--- it_company_task_service/settings/prod.py
+++ it_company_task_service/settings/prod.py
@@ -3,9 +3,11 @@ from .base import *
 
 DEBUG = False
 
+ALLOWED_HOSTS = []
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
-
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 DATABASES = {
    'default': {
